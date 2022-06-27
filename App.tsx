73df164386fb8aeb33de7792e01b3e4a63c076f7
Darshan Ponikar <darshanp@geekyants.com>
2022-06-27 15:08:26 +0530

    feat: ios functioanlity implemented

--- App.tsx
+++ App.tsx
@@ -3,8 +3,12 @@ import {Button, Text, View} from 'react-native';
 import NativeMyDeviceInfo from './js/NativeMyDeviceInfo';
 
 const App = () => {
-  const onPress = () => {
+  const onPress = async () => {
     console.log('I GOT THE IP ADDRESS', NativeMyDeviceInfo.getIpAddress());
+    console.log('GOT THE MAC ADDRESS', NativeMyDeviceInfo.getMacAddress());
+
+    const isEmulator = await NativeMyDeviceInfo.isEmulator();
+    console.log('IS EMULATOR', isEmulator);
   };
 
   return (
--- ios/MyDeviceInfo/MyDeviceInfo.mm
+++ ios/MyDeviceInfo/MyDeviceInfo.mm
@@ -8,6 +8,15 @@
 #import <Foundation/Foundation.h>
 #import <MyDeviceInfo/MyDeviceInfo.h>
 
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#import <mach/mach.h>
+#import <mach-o/arch.h>
+#import <CoreLocation/CoreLocation.h>
+#import <React/RCTUtils.h>
+#import <DeviceCheck/DeviceCheck.h>
+#import "EnvironmentUtil.h"
+
 @interface MyDeviceInfo: NSObject<NativeMyDeviceInfoSpec>
 @end
 
@@ -18,15 +27,55 @@ using namespace facebook;
 RCT_EXPORT_MODULE()
 
 - (NSString *)getIpAddress {
-  return @("Hello world");
+  NSString *address = @"0.0.0.0";
+     struct ifaddrs *interfaces = NULL;
+     struct ifaddrs *temp_addr = NULL;
+     int success = 0;
+     // retrieve the current interfaces - returns 0 on success
+     success = getifaddrs(&interfaces);
+     if (success == 0) {
+         // Loop through linked list of interfaces
+         temp_addr = interfaces;
+         while(temp_addr != NULL) {
+             sa_family_t addr_family = temp_addr->ifa_addr->sa_family;
+             // Check for IPv4 or IPv6-only interfaces
+             if(addr_family == AF_INET || addr_family == AF_INET6) {
+                 NSString* ifname = [NSString stringWithUTF8String:temp_addr->ifa_name];
+                     if(
+                         // Check if interface is en0 which is the wifi connection the iPhone
+                         // and the ethernet connection on the Apple TV
+                         [ifname isEqualToString:@"en0"] ||
+                         // Check if interface is en1 which is the wifi connection on the Apple TV
+                         [ifname isEqualToString:@"en1"]
+                     ) {
+                         const struct sockaddr_in *addr = (const struct sockaddr_in*)temp_addr->ifa_addr;
+                         socklen_t addr_len = addr_family == AF_INET ? INET_ADDRSTRLEN : INET6_ADDRSTRLEN;
+                         char addr_buffer[addr_len];
+                         // We use inet_ntop because it also supports getting an address from
+                         // interfaces that are IPv6-only
+                         const char *netname = inet_ntop(addr_family, &addr->sin_addr, addr_buffer, addr_len);
+
+                          // Get NSString from C String
+                         address = [NSString stringWithUTF8String:netname];
+                     }
+             }
+             temp_addr = temp_addr->ifa_next;
+         }
+     }
+     // Free memory
+     freeifaddrs(interfaces);
+     return address;
 }
 
 - (NSString *)getMacAddress {
-  return @("I am mac address");
+   if ([[UIDevice currentDevice] respondsToSelector:@selector(identifierForVendor)]) {
+         return [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+     }
+  return @"";
 }
 
 - (void)isEmulator:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject {
-  resolve(@YES);
+  resolve(TARGET_IPHONE_SIMULATOR ? @YES : @NO);
 }
 
 - (std::shared_ptr<react::TurboModule>)getTurboModule:(const react::ObjCTurboModule::InitParams &)params {
